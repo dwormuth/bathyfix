d55dd13fe92bcd71034bb4b1e4147bd5e445aaba
dwormuth <dwormuth@post.harvard.edu>
2023-07-13 13:47:50 -0400

    Using full paths

--- bathyfix.py
+++ bathyfix.py
@@ -10,39 +10,39 @@ StartTag = "<gpxtpx:depth>"
 EndTag = "</gpxtpx:depth>"
 StartTagLen = len(StartTag)
 EndTagLen = len(EndTag)
-adjustment = 99
+#adjustment = 99
 
 # 7/10/23 DWW changing to using argparse per page 275 of Python in a Nutshell, v4
 ap = argparse.ArgumentParser(description="Adjust the depths in GPX files using meters, negative number are below normal.")
 ap.add_argument("-d","--depth",help="Enter adjustment in meters, negative are below normal", type=float,
                 required=True, default=0.0)
-ap.add_argument("-f","--file", required=True, help="File to read")
+ap.add_argument("-f","--file", required=True, help="File to read without extension (.GPX assumed)")
 ns = ap.parse_args()
 print(ns)
 
 adjustment = ns.depth
 file2read = ns.file
 
-if adjustment == 99:
-    print("Water level offsets are positive if above normal and negative if below normal.")
-    try:
-        adjustment = float(input('Enter water level offset in meters: '))
-        print(adjustment)
-    except ValueError:
-        print('Enter a valid float')
-        exit(996)
+#if adjustment == 99:
+#    print("Water level offsets are positive if above normal and negative if below normal.")
+##    try:
+#        adjustment = float(input('Enter water level offset in meters: '))
+#        print(adjustment)
+#    except ValueError:
+#        print('Enter a valid float')
+#        exit(996)
 
 # Using readlines()
-infile = file2read + ".gpx"
+infile = "/Users/dwormuth/Documents/GIS_Data/ReefMaster/LongLakeBathy" + file2read[2:] + "/" + file2read + ".gpx"
 txt_path = Path(infile)
-print(txt_path)
+#print(txt_path)
 # 7/10/23 DWW Testing file existence
 
 if not os.path.exists(infile):
     print("Input file does not exist")
     exit(999)
 
-outfile = file2read + "_n.gpx"
+outfile = "/Users/dwormuth/Documents/GIS_Data/ReefMaster/LongLakeBathy" + file2read[2:] + "/" + file2read + "_n.gpx"
 txt_path = Path(outfile)
 txt_path.touch(exist_ok=True)
 if not os.path.exists(outfile):
